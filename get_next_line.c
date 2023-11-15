60adae2a401280479307d76b1819edc67029c642
Xavier Riera Carrion <xriera-c@c1r4p3.hive.fi>
2023-11-15 17:19:18 +0200

    Add first non-working draft

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 13:54:04 by xriera-c          #+#    #+#             */
+/*   Updated: 2023/11/15 16:18:42 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	*line;
+	size_t	size;
+
+	size = get_next_line_size(fd);
+	line = malloc(sizeof(char) * size + 1);
+	if (!line)
+		return (0);
+	read(fd, line, size);
+	return (line);
+}
